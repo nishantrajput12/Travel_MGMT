e6a00c88d756f76e44597f4510c1759cb24a86a3
nishantrajput12 <nishantrajput372@gmail.com>
2024-08-05 17:12:49 +0530

    Uploading files

---
+++ common/footer.php
@@ -0,0 +1,63 @@
+<!-- FOOTER SECTION STARTS -->
+					
+				<div class="footer col-sm-12">
+					<div class="col-sm-4">
+						
+						<div class="footerHeading">
+							Contact Us
+						</div>
+							
+						<div class="footerText">
+							ALVAS, Mijar <br> Karnataka, India
+						</div>
+				
+						<div class="footerText">
+							E-mail: nishant@tourism_management.com
+						</div>
+						
+					</div>
+					
+					
+					<div class="col-sm-4">
+					</div>
+				
+					
+					<div class="col-sm-4">
+					
+						<div class="footerHeading">
+							Social Links
+						</div>
+						
+						<div class="socialLinks">
+						
+							<div class="fb">
+								facebook.com/tourism_management
+							</div>
+						
+							<!-- <div class="gp">
+								www.google.com/tourism_management
+							</div>
+						 -->
+							<div class="tw">
+								twitter.com/tourism_management
+							</div>
+						
+							<div class="in">
+								linkedin.com/tourism_management
+							</div>
+						
+						</div> <!-- social links -->
+						
+					</div>
+						
+					<div class="col-sm-12">
+					<div class="copyrightContainer">
+						<div class="copyright">
+						Copyright &copy; 2024 Nishant
+						</div>
+					</div>
+					</div>
+							
+				</div> <!-- footer -->
+				
+			<!-- FOOTER SECTION ENDS -->
